3b02dea0da112c6c6fcf4bde06583d317b02f73a
TOUEi10 <52106355+touei10@users.noreply.github.com>
2019-08-29 22:27:37 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 'use strict';
+//fibonacci number
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
+function fib(n) {
+    if (memo.has(n)) {      // has関数は値を持っているかどうかをチェック
+        return memo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(i + `番目:` + fib(i));
+}
+
+
+/*
+//fibonacci number
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
@@ -14,7 +36,8 @@ function fib(n) {
 }
 
 const length = 40;
-for (let i = 0; i <= length; i++){
-   let result = fib(i);
+for (let i = 0; i <= length; i++) {
+    let result = fib(i);
     console.log(result + ".");
 }
+*/
